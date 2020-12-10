5576ecc06af7aec73b6a07122c2f059b1d886ff5
kelvindravin <kelvindravin@users.noreply.github.com>
2020-12-10 09:16:17 +0700

    [UPDATE] Default Home

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'C_Home';
+$route['default_controller'] = 'C_Home/loadHome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
