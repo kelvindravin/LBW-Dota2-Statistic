ae476a83b0280cb35080ce749f3bb132ff0db1d5
Harry Senjaya <44296024+harrysenjaya@users.noreply.github.com>
2021-01-07 11:02:56 +0700

    Update heroes.php

--- application/views/heroes.php
+++ application/views/heroes.php
@@ -20,13 +20,13 @@
         $bootstrapColWidth = 12 / $numOfCols;
         foreach ($hero as $key => $value) {
             if($rowCount % $numOfCols == 0) { 
-                ?> <div class="row"> <?php 
+                ?> <div class="row my-5 mx-5"> <?php 
             }
         $rowCount++; ?>
         <div class="col-md-<?php echo $bootstrapColWidth; ?>">
-        <div class="thumbnail">
+        <div class="thumbnail card mx-5 text-center">
             <?php 
-                echo $value->name.' <br> '. 
+                echo '<img src="./assets/images/'.$value->name.'.jpg" style="margin: 0 auto; width=128px; height=72px; "> <br> '. 
                 $value->localized_name.' <br> '.
                 $value->primary_attr.' <br> '.
                 $value->attack_type.' <br> '.
